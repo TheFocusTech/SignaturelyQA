bcf59a29fdba4cde4d39d9226a594d9802286b29
YuliaMatusevich <115679291+YuliaMatusevich@users.noreply.github.com>
2024-04-08 10:30:55 -0500

    Add project structure and test sample (#3)
    * Add project structure and first test

---
+++ src/test/java/runner/ProjectProperties.java
@@ -0,0 +1,68 @@
+package runner;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+public class ProjectProperties {
+
+    private static final String ENV_ACCESS_OPTIONS = "ACCESS_OPTIONS";
+    private static final String ENV_BROWSER_OPTIONS = "BROWSER_OPTIONS";
+    private static Properties properties;
+
+    private static void initProperties() {
+        properties = new Properties();
+        if (isServerRun()) {
+            if (System.getenv(ENV_ACCESS_OPTIONS) != null) {
+                for (String option : System.getenv(ENV_ACCESS_OPTIONS).split(";\n")) {
+                    String[] webOptionArr = option.split("=");
+                    properties.setProperty(webOptionArr[0], webOptionArr[1]);
+                }
+            }
+            if (System.getenv(ENV_BROWSER_OPTIONS) != null) {
+                for (String option : System.getenv(ENV_BROWSER_OPTIONS).split(";\n")) {
+                    String[] browserOptionArr = option.split("=");
+                    properties.setProperty(browserOptionArr[0], browserOptionArr[1]);
+                }
+            }
+        } else {
+            String[] fileNames = {"browser.properties", "access.properties"};
+
+            for (String fileName : fileNames) {
+                try {
+                    FileInputStream fileInputStream = new FileInputStream("./src/test/resources/" + fileName);
+                    properties.load(fileInputStream);
+                } catch (IOException e) {
+                    System.out.println("ERROR: The \u001B[31m" + fileName + "\u001B[0m file not found.");
+                    System.out.println("You need to create it from " + fileName + ".TEMPLATE file.");
+                    System.exit(1);
+                }
+            }
+        }
+    }
+
+    static {
+        initProperties();
+    }
+
+    public static void verifyPropertiesValues() {
+        for (String value : properties.stringPropertyNames()) {
+            if (properties.getProperty(value) == null || properties.getProperty(value).trim().isEmpty()) {
+                System.out.println("ERROR OCCURRED: \"" + value + "\" property value is invalid");
+                System.exit(1);
+            }
+        }
+    }
+
+    public static String getPropertyValue(String propertyName) {
+        if (!properties.containsKey(propertyName)) {
+            System.out.println("ERROR OCCURRED: \"" + propertyName + "\" property does not exist.");
+            System.exit(1);
+        }
+        return properties.getProperty(propertyName).trim();
+    }
+
+    static boolean isServerRun() {
+        return System.getenv("CI_RUN") != null;
+    }
+}
