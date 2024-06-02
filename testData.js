5aea46e82cbd36cfd58bcfd0857751c741c11da5
Elena Nov <93733106+ElNov686@users.noreply.github.com>
2024-06-02 10:25:22 +0100

    TC_04_11_02 | Sign Document | Verify custom signing order (#13)
    * Draft
    
    * Draft2
    
    * second commit
    
    * second commit

--- testData.js
+++ testData.js
@@ -4,5 +4,11 @@ export const URL_END_POINTS = {
 };
 export const ACTIVE_COLOR = 'rgb(0, 163, 250)';
 export const FILL_FOLDER_NAME = 'Folder 1';
+export const CHOOSE_SIGNERS_FIELDS = {
+    name1: 'John Doe',
+    email1: '5kXUw@example.com',
+    name2: 'John Doe2',
+    email2: '5kXUw@example.com'
+}
 
 
