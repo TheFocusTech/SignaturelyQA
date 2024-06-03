7fe7eddb300abcfa90d8cde628f0dbb4bf85fae2
Vera Fesianava <verafes@gmail.com>
2024-06-03 09:21:29 -0700

    TC_12_48_01_01 | Verify User can copy API key created by Create API button on the right (#17)
    * TC_12_48_01_01 | Verify User can copy API key created by Create API button on the right.

--- page_objects/settingsCompanyPage.js
+++ page_objects/settingsCompanyPage.js
@@ -1,4 +1,5 @@
 import SettingsBillingPage from "./settingsBillingPage";
+import SettingsAPIPage from "./settingsAPIPage";
 
 class SettingsCompanyPage {
     constructor(page){
@@ -6,7 +7,8 @@ class SettingsCompanyPage {
     }
 
     locators = {
-        getSettingsBillingSidebarLink: () => this.page.getByRole('link', {name: 'Billing', exact: true}).first()
+        getSettingsBillingSidebarLink: () => this.page.getByRole('link', {name: 'Billing', exact: true}).first(),
+        getAPILink: () => this.page.getByRole('link', { name: 'API' }),
     }
    
     async clickSettingsBillingSidebarLinkAngGoSettingsBillingPage() {
@@ -14,6 +16,11 @@ class SettingsCompanyPage {
 
         return new SettingsBillingPage(this.page);
     }
-    
+
+    async clickAPILinkAndGoAPIPage(){
+        await this.locators.getAPILink().click();
+
+        return new SettingsAPIPage(this.page);
+    }
 }
 export default SettingsCompanyPage;
