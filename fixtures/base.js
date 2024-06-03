94453271160506c98b7b121f515444dd5935ede9
Irena Khitrushka <109556395+IrenaKh95@users.noreply.github.com>
2024-06-03 15:30:56 +0200

    RF [TC_06_24_01] (#15)
    * RF TC_06_24_01
    
    * RF: fixed fixtures, add empty trash step

--- fixtures/base.js
+++ fixtures/base.js
@@ -1,18 +1,16 @@
-import { test as base } from '@playwright/test';
-import LoginPage from '../page_objects/loginPage';
-import SignPage from '../page_objects/signPage';
-import TemplatesActivePage from '../page_objects/templatesActivePage';
+import { test as base } from "@playwright/test";
+import LoginPage from "../page_objects/loginPage";
+import SignPage from "../page_objects/signPage";
 
 const EMAIL = process.env.USER_EMAIL;
 const PASSWORD = process.env.USER_PASSWORD;
 
 export const test = base.extend({
-
     loginBusinessUser: [
         async ({ page }, use) => {
             const loginPage = new LoginPage(page);
 
-            await page.goto('/');
+            await page.goto("/");
             await loginPage.fillEmailAddressInputField(EMAIL);
             await loginPage.fillPasswordInputField(PASSWORD);
             await loginPage.clickLoginAndGoSignPage();
@@ -24,16 +22,18 @@ export const test = base.extend({
 
     createNewFolder: [
         async ({ page }, use) => {
-            const templatesActivePage = new TemplatesActivePage(page);
             const signPage = new SignPage(page);
-            
-            await signPage.clickTemplateDropdownAndGoTemplatesActivePage();  
-            await templatesActivePage.clickCreateFolderBtn();
-            await templatesActivePage.fillNewFolderNameInputField();
-            await templatesActivePage.clickCreateBtn();
+
+            const documentsPage = await signPage.clickDocumentsDropdownAndGoDocumentsPage();
+            await documentsPage.clickCreateFolderBtn();
+            await documentsPage.fillNewFolderNameInputField();
+            await documentsPage.clickCreateBtn();
+            await documentsPage.clickSignaturelyLogoAndGoSignPage();
+            await documentsPage.locators.getToaster().waitFor({ state: "visible" });
+            await documentsPage.locators.getToaster().waitFor({ state: "hidden" });
 
             await use("");
         },
         { scope: "test" },
-    ]
+    ],
 });
