ea119f6aa839cb1b383de97d235aa35b832bd74a
teya-teya <teya.teya89@yandex.ru>
2024-06-04 00:48:21 +0300

    add test 'create and delete signature'

--- tests/navigation.spec.js
+++ tests/navigation.spec.js
@@ -30,4 +30,28 @@ test('create signature', async ({page, loginBusinessUser, deleteSignature}) => {
   await page.locator('.sidebar__item-trigger-inner').first().click();
   await page.waitForURL('https://staging.d2twwklgqmrfet.amplifyapp.com/sign')
   
+});
+
+test('create and delete signature', async ({page, loginBusinessUser}) => {
+  test.setTimeout(35000)
+  await page.locator('.dropDownUser__wrapper').click()
+  await page.getByRole('link', {name: 'Edit Signature'}).click()
+  await page.getByRole('button', {name: 'Create Signature'}).click();
+  await page.locator('.form__input').first().fill('Godrik Horn');
+  await page.locator('.form__input').nth(1).fill('GH');
+  await page.locator('.uiCheckbox__inner.uiCheckbox--unChecked').click();
+  await page.getByRole('button', {name: 'Create Signature'}).click();
+  
+  await expect (page.locator('.settingsSignature__item-inner').first()).toBeVisible();
+  
+  await page.locator('[type="success"]').click();
+  await page.locator('.sidebar__item-trigger-inner').first().click();
+
+  await page.locator('.dropDownUser__wrapper').click();
+  await page.getByRole('link', {name: 'Edit Signature'}).click();
+  await page.locator('.settingsSignature__dropDown-trigger').first().click();
+  await page.locator('.settingsSignature__dropDown-item--delete').click();
+  await page.getByRole('button', {name: 'Delete'}).click();
+  await page.locator('[type="success"]').click();
+  await page.locator('.sidebar__item-trigger-inner').first().click();
 })
