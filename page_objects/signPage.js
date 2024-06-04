02576d8dd4ab843a94c3e7da43fe254dc43b8370
Elena Nov <93733106+ElNov686@users.noreply.github.com>
2024-06-04 19:44:36 +0100

    fix added

--- page_objects/signPage.js
+++ page_objects/signPage.js
@@ -43,14 +43,14 @@ class SignPage {
         return new DocumentsPage(this.page);
     }
 
-    clickUploadFileBtn(file) {
-        this.locators.getFileInputField().setInputFiles(file);
+    async clickUploadFileBtn(file) {
+        await this.locators.getFileInputField().setInputFiles(file);
 
         return this;
     }
 
-    clickPrepareDocumentBtn() {
-        this.locators.getPrepareDocumentBtn().click();
+    async clickPrepareDocumentBtn() {
+        await this.locators.getPrepareDocumentBtn().click();
 
         return this;
     }
