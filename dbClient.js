9da499d24ceb765d9be12880c70a718dc805f9c3
YuliaMatusevich <yuliya.matusevich.qa@gmail.com>
2024-06-05 22:18:15 -0500

    check port

--- dbClient.js
+++ dbClient.js
@@ -3,7 +3,7 @@ const { Client } = require('pg');
 export const client = new Client({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
-    port: process.env.DB_PORT,
+    port: '5432',
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME
 });
