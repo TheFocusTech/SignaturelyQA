8231f3f6a82169c809cbb07584e747285602f5e5
MlimaU <129126676+mlimau@users.noreply.github.com>
2024-06-07 12:41:32 -0400

    Updated with Sign Page

--- tests/08Forms.spec.js
+++ tests/08Forms.spec.js
@@ -3,7 +3,6 @@ import { test, loginBusinessUser, createForm } from '../fixtures/base.js'
 import LoginPage from '../page_objects/loginPage.js'
 import FormRequestsPage from '../page_objects/formRequestsPage.js'
 import FormRequestsCreatePage from '../page_objects/formRequestsCreatePage.js'
-import SignPage from '../page_objects/signPage.js'
 import { FORM_NAME, OPTIONAL_MESSAGE_TEXT, FORM_NAME_EDIT, OPTIONAL_MESSAGE_TEXT_EDIT } from '../testData.js'
 import FormRequestsEditPage from "../page_objects/formRequestsEditPage.js"
 
@@ -11,15 +10,10 @@ test.describe('Forms', () => {
 
   test('TC_08_33_01 | "Forms" - verify that user can edit form.', async ({ page, loginBusinessUser, createForm }) => {
     test.slow()
-    const signPage = new SignPage(page);
-    const formRequestsPage = new FormRequestsPage(page);
-         
-
-
+      const formRequestsPage = new FormRequestsPage(page);
       await formRequestsPage.clickOptionsDropdown();
       await formRequestsPage.locators.getEditDropdownAndGoFormRequestsEditPage().waitFor();
       const formRequestsEditPage = await formRequestsPage.clickEditDropdownAndGoFormRequestsEditPage();
-
 
       await formRequestsEditPage.fillFormNameInputField(FORM_NAME_EDIT);
       await formRequestsEditPage.fillOptionalMessageInputField(OPTIONAL_MESSAGE_TEXT_EDIT);
@@ -37,11 +31,8 @@ test.describe('Forms', () => {
       await expect(formRequestsPage.locators.getFormSavedToasterMessage()).toBeVisible();
       await expect(formRequestsPage.locators.getFirstFormTitle()).toHaveText(FORM_NAME_EDIT);
 
-
       await formRequestsPage.clickCheckBoxOfForm();
       await formRequestsPage.clickDeleteBtn();
       await formRequestsPage.clickYesDeleteModalWindowBtn();
-
-      await signPage.clickSignSidebarLinkAndGoSignPage();
-    })
+     })
 })
