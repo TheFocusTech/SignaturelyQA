8529a73c1b4b06d9ef43d29cb6ac56aa957e87cb
YuliaMatusevich <yuliya.matusevich.qa@gmail.com>
2024-06-08 01:11:14 -0400

    remofe skip and rf tests with create ser

--- tests/navigation.spec.js
+++ tests/navigation.spec.js
@@ -1,22 +1,22 @@
 import { expect } from '@playwright/test';
-import { test } from "../fixtures/base.js";
+import { test, createBusinessUserAndLogin } from "../fixtures/base.js";
 import { URL_END_POINTS, DATA_SIGNER, EMPTY_DOCUMENTS_HEADER, EMPTY_TRASH_HEADER } from '../testData.js';
 import SignPage from '../page_objects/signPage.js';
 
 const BASE_URL = process.env.URL;
 
-test.skip('Navigate to login page', async ({ page }) => {
+test('Navigate to login page', async ({ page }) => {
     await page.goto('/');
 
     await expect(page).toHaveTitle('Log In | Signaturely');
 });
 
-test.skip('Check login fixture', async ({ page, loginBusinessUser }) => {
+test('Check login fixture', async ({ page, loginBusinessUser }) => {
 
     await expect(page).toHaveURL(BASE_URL + URL_END_POINTS.signEndPoint);
 });
 
-test.skip('Create signature', async ({ page, loginBusinessUser, deleteSignature }) => {
+test('Create signature', async ({ page, createBusinessUserAndLogin, deleteSignature }) => {
     const signPage = new SignPage(page);
     await signPage.clickDropDownUser();
     const editSignature = await signPage.clickEditSignatureAndGoEditSignaturePage();
@@ -33,7 +33,7 @@ test.skip('Create signature', async ({ page, loginBusinessUser, deleteSignature 
     await page.waitForURL('https://staging.d2twwklgqmrfet.amplifyapp.com/sign');
 });
 
-test.skip('Create and delete signature', async ({ page, loginBusinessUser }) => {
+test('Create and delete signature', async ({ page, createBusinessUserAndLogin }) => {
     const signPage = new SignPage(page);
     await signPage.clickDropDownUser();
     const editSignature = await signPage.clickEditSignatureAndGoEditSignaturePage();
@@ -59,7 +59,7 @@ test.skip('Create and delete signature', async ({ page, loginBusinessUser }) => 
     await editSignature.clickSignSidebarLinkAndGoSignPage();
 });
 
-test.skip('check clean documents fixture', async ({ page, loginBusinessUser}) => {
+test('check clean documents fixture', async ({ page, createBusinessUserAndLogin}) => {
 
     const signPage = new SignPage(page);
 
