b859a3e5139bf61b5d141dbbe5179c8154fcb4a5
YuliaMatusevich <yuliya.matusevich.qa@gmail.com>
2024-06-11 11:06:51 -0500

    rf test, add create user, delete clean

--- tests/11Profile.spec.js
+++ tests/11Profile.spec.js
@@ -1,13 +1,12 @@
 import { expect } from '@playwright/test';
-import { test, loginBusinessUser } from "../fixtures/base.js";
-import { changeUserPasswordApiAdmin } from '../helpers/apiCalls.js';
+import { test } from "../fixtures/base.js";
 import SignPage from "../page_objects/signPage";
-import { TOASTER_MESSAGE, URL_END_POINTS } from '../testData.js';
-import { generateRandomPassword } from '../helpers/utils.js'
+import { TOASTER_MESSAGE } from '../testData.js';
+import { generateRandomPassword } from '../helpers/utils.js';
 
 test.describe('Profile', () => {
 
-    test('TC_11_45_01 | Verify User can update a password', async ({ page, loginBusinessUser, request }) => {
+    test('TC_11_45_01 | Verify User can update a password', async ({ page, createBusinessUserAndLogin }) => {
         const newPassword = generateRandomPassword(25);
         console.log(newPassword);
 
@@ -22,19 +21,5 @@ test.describe('Profile', () => {
         await expect(settingsProfilePage.locators.getToastBody()).toHaveText(TOASTER_MESSAGE.profileUpdated);
         await settingsProfilePage.waitForToasterHidden();
         expect(await settingsProfilePage.locators.getToastBody().isHidden()).toBe(true);
-        const loginPage = await settingsProfilePage.clickSignSidebarMenuOptionAndGoToLoginPageAfterUpdatePassword();
-        await expect(page).toHaveURL('https://staging.d2twwklgqmrfet.amplifyapp.com/login');
-
-        const updatedUserCredentials = {
-            email: process.env.USER_EMAIL,
-            password:`${newPassword}`
-        };
-
-        const responce = await changeUserPasswordApiAdmin(request, updatedUserCredentials);
-        await expect(responce).toBeOK();
-        await loginPage.fillEmailAddressInputField(process.env.USER_EMAIL);
-        await loginPage.fillPasswordInputField(process.env.USER_PASSWORD);
-        await loginPage.clickLoginAndGoSignPage();
-        await expect(page).toHaveURL(process.env.URL + URL_END_POINTS.signEndPoint);
     })
 })
