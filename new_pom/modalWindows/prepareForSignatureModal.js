1bf83f78d649219feb11a3b91f587bfc3fc60838
Elena Nov <93733106+ElNov686@users.noreply.github.com>
2024-06-15 20:48:34 +0300

    initial commit

--- new_pom/modalWindows/prepareForSignatureModal.js
+++ new_pom/modalWindows/prepareForSignatureModal.js
@@ -2,9 +2,13 @@ export default class PrepareForSignatureModal {
   constructor(page) {
     this.page = page;
     this.signDocumentRadioBtn = this.page.getByText('Sign a Document', { exact: true });
+    this.signAndSendForSignatureRadioBtn = this.page.getByText('Sign & Send for Signature', { exact: true });
     this.continueBtn = this.page.getByRole('button', { name: 'Continue' });
     this.gotItBtn = this.page.getByRole('button', { name: 'Got it' });
     this.signFieldsItem = this.page.locator('.interactModal__fieldBar-fieldItem-icon').first();
+    this.addOtherSigners = this.page.getByText('Add signer', { exact: true });
+    this.addOtherSignersNameField = this.page.getByText('Name', { exact: true });
+    this.addOtherSignersEmailField = this.page.getByText('Email', { exact: true });
 
   }
 
@@ -22,6 +26,22 @@ export default class PrepareForSignatureModal {
 
   async clickGotItBtn() {
     await this.gotItBtn.click();
+  }
+
+  async clickSignAndSendForSignatureRadioBtn() {
+    await this.signAndSendForSignatureRadioBtn.click();
+  }
+
+  async clickAddOtherSignersBtn() {
+    await this.addOtherSigners.click();
+  }
+
+  async fillkAddOtherSignersNameField(name) {
+    await this.addOtherSignersNameField.fill(name)
+  }
+
+  async fillAddOtherSignersEmailField(email) {
+    await this.addOtherSignersEmailField.fill(email)
   }
 
 
--- tests/04SignDocument.spec.js
+++ tests/04SignDocument.spec.js
@@ -1,34 +1,48 @@
 import { expect } from "@playwright/test";
-import {test, createBusinessUserAndLogin} from "../fixtures/base.js";
-import SignPage from "../page_objects/signPage";
-const EMAIL = process.env.USER_EMAIL;
-const PASSWORD = process.env.USER_PASSWORD;
-const BASE_URL = process.env.URL;
-import {CHOOSE_SIGNERS_FIELDS} from '../testData.js';
+import { test, createBusinessUserAndLogin, signPage, prepareForSignature } from "../fixtures/base.js";
+import { name1, email1 } from "../testData.js";
 
-test.describe('SignDocument', () => {
+test.describe('CreateDocument', () => {
 
-    test('TC_04_11_02 | Verify custom signing order', async ({page,createBusinessUserAndLogin}) => {
-        const signPage = new SignPage(page);
-        await signPage.clickUploadFileBtn('testDocuments/picture.jpg');
+  test.only('TC_04_11_02 | Verify custom signing order', async ({ createBusinessUserAndLogin, signPage, prepareForSignature }) => {
 
-        await signPage.locators.getPrepareDocumentBtn().waitFor({state: 'visible'});
-        await signPage.clickPrepareDocumentBtn();
+    await signPage.UploadFileOnSignPage.fileUploader.uploadFile('testDocuments/picture.jpg');
+    await signPage.UploadFileOnSignPage.fileUploader.waitForVisibleProgressBar();
+    await signPage.UploadFileOnSignPage.fileUploader.waitForHiddenProgressBar();
+    await signPage.UploadFileOnSignPage.clickPrepareDocumentBtn();
 
-        await signPage.clickSendForSignatureRadioBtn();
-        await signPage.clickAddSignerBtn();
+    await prepareForSignature.clickSignAndSendForSignatureRadioBtn();
+    await prepareForSignature.clickAddOtherSignersBtn();
 
-        await signPage.fillChooseSignersNameField(CHOOSE_SIGNERS_FIELDS.name1);
-        await signPage.fillChooseSignersEmailField(CHOOSE_SIGNERS_FIELDS.email1);
-
-        await signPage.clickAddSignerBtn();
-
-        await signPage.fillChooseSignersNameField(CHOOSE_SIGNERS_FIELDS.name2);
-        await signPage.fillChooseSignersEmailField(CHOOSE_SIGNERS_FIELDS.email2);
-
-        await signPage.clickCustomSigningOrderCheckbox();
-
-        await expect(signPage.locators.getCustomSigningOrderPositionNumberOne()).toBeVisible();
-        await expect(signPage.locators.getCustomSigningOrderPositionNumberTwo()).toBeVisible();
-    })
+    // await prepareForSignature.fillkAddOtherSignersNameField(name1);
+    // await prepareForSignature.fillAddOtherSignersEmailField(email1);
+  })
 })
+
+
+// test.describe('SignDocument', () => {
+
+//     test('TC_04_11_02 | Verify custom signing order', async ({page,createBusinessUserAndLogin}) => {
+//         const signPage = new SignPage(page);
+//         await signPage.clickUploadFileBtn('testDocuments/picture.jpg');
+
+//         await signPage.locators.getPrepareDocumentBtn().waitFor({state: 'visible'});
+//         await signPage.clickPrepareDocumentBtn();
+
+//         await signPage.clickSendForSignatureRadioBtn();
+//         await signPage.clickAddSignerBtn();
+
+//         await signPage.fillChooseSignersNameField(CHOOSE_SIGNERS_FIELDS.name1);
+//         await signPage.fillChooseSignersEmailField(CHOOSE_SIGNERS_FIELDS.email1);
+
+//         await signPage.clickAddSignerBtn();
+
+//         await signPage.fillChooseSignersNameField(CHOOSE_SIGNERS_FIELDS.name2);
+//         await signPage.fillChooseSignersEmailField(CHOOSE_SIGNERS_FIELDS.email2);
+
+//         await signPage.clickCustomSigningOrderCheckbox();
+
+//         await expect(signPage.locators.getCustomSigningOrderPositionNumberOne()).toBeVisible();
+//         await expect(signPage.locators.getCustomSigningOrderPositionNumberTwo()).toBeVisible();
+//     })
+// })
