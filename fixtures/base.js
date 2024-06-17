243ab857da8b5c1663614f3a241ee2112954356a
ViktoriyaED <115054456+ViktoriyaED@users.noreply.github.com>
2024-06-17 14:19:48 -0400

    GmailApi & TC_01_01_02  | Verify that user redirects to the Activate-trial page (#53)
    * * Add Gmail index.js
    * Add apiCalls.js - signUpRequest(),
    * Add delay(), generateNewUserData(), createNewUserThroughApi(), retrieveUserEmailConfirmationLink() to utils.js
    * Add ActivateTrialStripePage
    * Add 01Registration.spec.js class with test

--- fixtures/base.js
+++ fixtures/base.js
@@ -7,6 +7,8 @@ import NewSignPage from '../new_pom/pages/sign/signPage.js';
 import NewDocumentsPage from '../new_pom/pages/documents/documentsPage.js';
 import NewDocumentsTrashPage from '../new_pom/pages/documents/documentsTrashPage.js';
 import PrepareForSignatureModal from '../new_pom/modalWindows/prepareForSignatureModal.js';
+import NewLoginPage from '../new_pom/pages/loginPage.js';
+import ActivateTrialStripePage from "../new_pom/pages/activateTrialStripePage";
 
 export const test = base.extend({    
 
@@ -64,5 +66,8 @@ export const test = base.extend({
         await use(new NewDocumentsTrashPage(page));
     },
 
+    activateTrialStripePage: async ({ page }, use) => {
+        await use(new ActivateTrialStripePage(page));
+    },
     
 });
