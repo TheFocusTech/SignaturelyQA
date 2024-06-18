245e160f02989b78c3bf5b3655989ba7263c7f65
Anastasia <142177100+teya-teya@users.noreply.github.com>
2024-06-18 04:34:23 +0300

    Update base.js

--- fixtures/base.js
+++ fixtures/base.js
@@ -7,6 +7,7 @@ import NewSignPage from '../new_pom/pages/sign/signPage.js';
 import NewDocumentsPage from '../new_pom/pages/documents/documentsPage.js';
 import NewDocumentsTrashPage from '../new_pom/pages/documents/documentsTrashPage.js';
 import PrepareForSignatureModal from '../new_pom/modalWindows/prepareForSignatureModal.js';
+import NewLoginPage from '../new_pom/pages/loginPage.js';
 import FinalStepModal from '../new_pom/modalWindows/finalStepModal.js';
 import ActivateTrialStripePage from "../new_pom/pages/activateTrialStripePage";
 import NewSettingsCompanyPage from "../new_pom/pages/settings/settingsCompanyPage.js";
