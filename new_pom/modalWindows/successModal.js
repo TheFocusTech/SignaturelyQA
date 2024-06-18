3a8e5b2c5a5a4d6c448f49c1bcc123c2237263f2
Anastasia <142177100+teya-teya@users.noreply.github.com>
2024-06-18 20:09:26 +0300

    Update successModal.js

--- new_pom/modalWindows/successModal.js
+++ new_pom/modalWindows/successModal.js
@@ -1,6 +1,6 @@
 export default class SuccessModal {
-  constructor(page) {
-      this.page = page;
+    constructor(page) {
+        this.page = page;
 
         this.statusDocument = this.page.getByText('Save a copy of your document');
         this.backToDocumentsBtn = this.page.getByRole('button', { name: 'Back to Documents' });
