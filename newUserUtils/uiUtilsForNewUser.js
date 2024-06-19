37f60e147d973c77e5c88296b58122e3c7f6ea57
IriSamo <i.samosuk@gmail.com>
2024-06-19 12:53:12 -0400

    TC_14_54_01 | Attach/delete payment card (#77)
    * Add test TC_14_54_01 | Attach/delete payment card: create SettingBillingPortalPage, StripeEnterPaymentDetailsPage; change format for constant CARD_DETAILS
    
    * Change time for test and method
    
    * Correct function attachCard in StripeEnterPaymentDetailsPage
    
    * Correct typos
    
    * Locators simplification

--- newUserUtils/uiUtilsForNewUser.js
+++ newUserUtils/uiUtilsForNewUser.js
@@ -1,7 +1,7 @@
 import { newUserNumber, newUserEmail, newUserPassword } from "./apiUtilsForNewUser";
 import LoginPage from "../page_objects/loginPage";
 import SignPage from "../page_objects/signPage";
-import { VISA_CARD_DATA } from '../testData.js';
+import { CARD_DETAILS } from '../testData.js';
 
 export async function newFreeUserLogin(page) {
     const loginPage = new LoginPage(page);
@@ -17,12 +17,7 @@ export async function upgradeFreeUserToBusinessAndLogin(page) {
     const settingsCompanyPage = await signPage.clickSettingsSidebarLinkAndGoSettingsCompanyPage();
     let settingsBillingPage = await settingsCompanyPage.clickSettingsBillingSidebarLinkAngGoSettingsBillingPage();
     const upgradeYourPlanModal = await settingsBillingPage.clickBusinessUpgradeAndGoToUpgradeYourPlanModal();
-    await upgradeYourPlanModal.fillCreditCardData(
-        VISA_CARD_DATA.cardNumber,
-        VISA_CARD_DATA.expirationDate,
-        VISA_CARD_DATA.cvc,
-        VISA_CARD_DATA.fullNameOnCard,
-        VISA_CARD_DATA.zip);
+    await upgradeYourPlanModal.fillCreditCardData(CARD_DETAILS.VISA);
     const specialOfferUpsellModal = await upgradeYourPlanModal.clickSubscribeButtonAnGoToSpecialOfferUpsellModal();
     const settingsBillingPlanPage = await specialOfferUpsellModal.cancelSpecialOfferAndGoToSettingsBillingPlanPage();
     await settingsBillingPlanPage.locators.getToasterPopup().isVisible();
