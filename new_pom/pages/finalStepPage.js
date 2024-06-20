0f97a1c892c85c8f6868b71eaffe4d67569af327
Eduard-QA <eduard.mickel@gmail.com>
2024-06-20 22:11:06 +0300

    reselv_conflict

--- new_pom/pages/finalStepPage.js
+++ new_pom/pages/finalStepPage.js
@@ -1,45 +1,34 @@
 import ToastComponent from "../components/toastComponent";
 
 export default class FinalStepPage {
-  constructor(page) {
+    constructor(page) {
 
-    this.page = page;
-    this.toast = new ToastComponent(this.page);
+        this.page = page;
+        this.toast = new ToastComponent(this.page);
 
-<<<<<<< HEAD
-    this.documentTitleField = this.page.getByPlaceholder("Enter the title");
-    this.signDocumentAndSendForSignatureBtn = this.page.getByRole("button", { name: "Sign Document and Send for Signature" });
-    this.sendForSignatureBtn = this.page.getByRole("button", { name: "Send for Signature" });
-=======
-    this.toastComponent = new ToastComponent(this.page)
-    this.documentTitleField = this.page.getByPlaceholder('Enter the title');
-    this.signDocumentAndSendForSignatureBtn = this.page.getByRole('button', { name: 'Sign Document and Send for Signature' });
-    this.sendForSignatureBtn = this.page.getByRole('button', { name: 'Send for Signature' }); 
-    this.documentOptionalMessageField = this.page.getByPlaceholder('Add an optional message for the document signers.');
-    this.signDocumentBtn = this.page.getByRole('button', { name: 'Sign Document' });
-    
+        this.documentTitleField = this.page.getByPlaceholder("Enter the title");
+        this.signDocumentAndSendForSignatureBtn = this.page.getByRole("button", { name: "Sign Document and Send for Signature" });
+        this.sendForSignatureBtn = this.page.getByRole("button", { name: "Send for Signature" });
+        this.documentOptionalMessageField = this.page.getByPlaceholder('Add an optional message for the document signers.');
+        this.signDocumentBtn = this.page.getByRole('button', { name: 'Sign Document' });
+
     }
 
->>>>>>> 5cd2d645e7833c3583e5c3e3346c7d97ed65bc02
-  }
+    async fillDocumentTitleField(title) {
+        await this.documentTitleField.waitFor({ status: "visible" });
+        await this.documentTitleField.fill(title);
+    }
 
-  async fillDocumentTitleField(title) {
-    await this.documentTitleField.waitFor({ status: "visible" });
-    await this.documentTitleField.fill(title);
-  }
+    async clickSignDocumentAndSendForSignatureBtn() {
+        await this.signDocumentAndSendForSignatureBtn.click();
+    }
 
-  async clickSignDocumentAndSendForSignatureBtn() {
-    await this.signDocumentAndSendForSignatureBtn.click();
-  }
 
-       
-  async clickSendForSignatureBtn() {
-    await this.toast.waitForToastDocumentSaved();
-    await this.sendForSignatureBtn.click();
-  }
-<<<<<<< HEAD
-}
-=======
+    async clickSendForSignatureBtn() {
+        await this.toast.waitForToastDocumentSaved();
+        await this.sendForSignatureBtn.click();
+    }
+
 
 
     async fillDocumentOptionalMessageField(message) {
@@ -51,4 +40,4 @@ export default class FinalStepPage {
     }
 
 }
->>>>>>> 5cd2d645e7833c3583e5c3e3346c7d97ed65bc02
+
