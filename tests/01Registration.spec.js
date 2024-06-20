dd85f71f233da4e6f3a72f84c4d838d34dbcfe05
ViktoriyaED <115054456+ViktoriyaED@users.noreply.github.com>
2024-06-20 17:34:26 -0400

    Add test TC_01_02_02 | Verify that Free user redirects to the SignPage after confirmation of the registration (#83)

--- tests/01Registration.spec.js
+++ tests/01Registration.spec.js
@@ -1,12 +1,17 @@
 import { expect } from "@playwright/test";
 import { test } from "../fixtures/base"
-import { retrieveUserEmailConfirmationLink, createNewUserThroughApi } from "../helpers/utils";
+import {
+    retrieveUserEmailConfirmationLink,
+    createNewUserThroughApi,
+    createNewFreeUserThroughApi,
+} from "../helpers/utils";
 import {
     URL_END_POINTS,
     START_YOUR_FREE_TRIAL_STATEMENT,
     BILLING_INFORMATION,
     BUSINESS_MONTHLY_PLAN,
-    CARD_DETAILS
+    CARD_DETAILS,
+    FREE_PLAN_DESCRIPTION
 } from "../testData";
 
 test('TC_01_01_02 | Verify that user redirects to the Activate-trial page', async ({page, request, activateTrialStripePage}) => {
@@ -35,3 +40,15 @@ test('TC_01_01_03 | Verify that user redirects to the homepage after filling Bil
     await settingsCompanyPage.sideMenuSettings.clickBilling();
     await expect(settingsBillingPage.billingPlanDescription).toHaveText(BUSINESS_MONTHLY_PLAN)
  })
+
+test('TC_01_02_02 | Verify that Free user redirects to the SignPage after confirmation of the registration', async({page, request, signPage, settingsCompanyPage, settingsBillingPage}) => {
+    const newUserData = await createNewFreeUserThroughApi(request);
+    const confirmationLink = await retrieveUserEmailConfirmationLink(request, newUserData);
+
+    await page.goto(confirmationLink);
+    await page.waitForURL(`${process.env.URL}${URL_END_POINTS.signEndPoint}`);
+
+    await signPage.sideMenu.clickSettings();
+    await settingsCompanyPage.horizontalMenu.clickBilling();
+    await expect(settingsBillingPage.billingPlanDescription).toHaveText(FREE_PLAN_DESCRIPTION);
+})
