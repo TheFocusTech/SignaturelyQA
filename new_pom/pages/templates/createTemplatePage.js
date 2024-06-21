16ec31310bfd654582e957613175bf0c84cac932
Katerina Poliit <alexispoliyit@gmail.com>
2024-06-21 15:02:18 +0300

    исправила, относительно комментариев

--- new_pom/pages/templates/createTemplatePage.js
+++ new_pom/pages/templates/createTemplatePage.js
@@ -7,36 +7,25 @@ export default class NewCreateTemplatePage {
         this.page = page;
         this.sideMenu = new SideMenuComponent(this.page);
         this.sideMenuTemplates = new SideMenuTemlatesComponent (this.page);
+        this.fileUploader = new FileUploaderComponent(this.page);
         this.templateNameField = this.page.getByPlaceholder('A template name to identify');
         this.optionalMessageField = this.page.getByPlaceholder('Add an optional message for');
         this.createTemplateRolesField = this.page.getByPlaceholder('Role');
         this.fillTemlateBtn = this.page.getByRole('button', { name: 'Fill Template' });
-        this.fileUploader = new FileUploaderComponent(this.page);
 
     }
 
-    async fillTemplateNameField(name) {
-        try {
-          await this.templateNameField.fill(name);
-        } finally {
 
-        }
-      }
+    async fillTemplateNameField(name) {
+      await this.templateNameField.fill(name);
+    }
 
       async fillOptionalMessageField(message) {
-        try {
-          await this.optionalMessageField.fill(message);
-        } finally {
-
-        }
+        await this.optionalMessageField.fill(message);
       }
 
       async fillCreateTemplateRolesField(role) {
-        try {
-          await this.createTemplateRolesField.fill(role);
-        } finally {
-
-        }
+        await this.createTemplateRolesField.fill(role);
       }
 
       async clickFillTemlateBtn() {
