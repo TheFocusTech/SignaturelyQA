7a690221c1947b4513d357332748bf7fcd280ec7
Eduard-QA <eduard.mickel@gmail.com>
2024-06-22 12:31:50 +0300

    create precondition "prepareForSignature"

--- helpers/preconditions.js
+++ helpers/preconditions.js
@@ -1,4 +1,5 @@
 import { DATA_SIGNER } from "../testData";
+import { CHOOSE_SIGNERS_FIELDS } from '../testData.js';
 
 export const createSignature = async (signPage, settingsCompanyPage, settingsEditSignaturePage, createOrEditSignatureOnSettingModal) => {
     await signPage.sideMenu.clickSettings();
@@ -9,4 +10,19 @@ export const createSignature = async (signPage, settingsCompanyPage, settingsEdi
     await createOrEditSignatureOnSettingModal.clickCheckboxAgree();
     await createOrEditSignatureOnSettingModal.clickCreateSignatureBtn();
     await settingsCompanyPage.sideMenu.clickSign();
+}
+
+export const prepareForSignature = async ( signPage, prepareForSignatureModal) => {
+    await signPage.uploadFile.fileUploader.uploadFile('testDocuments/picture.jpg');
+    await signPage.uploadFile.clickPrepareDocumentBtn();
+    await prepareForSignatureModal.clickSendForSignatureRadioBtn();
+    await prepareForSignatureModal.clickAddSignerBtn();
+    await prepareForSignatureModal.fillSignerNameField(CHOOSE_SIGNERS_FIELDS.name1, 0)
+    await prepareForSignatureModal.fillSignerEmailField(process.env.PREFIX_EMAIL + '01' + process.env.EMAIL_DOMAIN, 0);
+    await prepareForSignatureModal.clickContinueBtn();
+    await prepareForSignatureModal.clickGotItBtn();
+
+    await prepareForSignatureModal.clickSignFieldsItem();
+    await prepareForSignatureModal.doCanvasClicks();
+    await prepareForSignatureModal.clickSaveBtn();
 }
--- tests/05DocumentsTyped.spec.js
+++ tests/05DocumentsTyped.spec.js
@@ -1,35 +1,23 @@
 
 import { expect } from "@playwright/test";
 import { test } from "../fixtures/base.js";
-import { CHOOSE_SIGNERS_FIELDS } from '../testData.js';
+import { prepareForSignature } from "../helpers/preconditions.js";
 
 test.describe('DocumentsType', () => {
 
-  test('TC_05_21_01 | Verify that button "Edit&Resend" is active', async ({ createBusinessUserAndLogin, signPage, prepareForSignatureModal, successModal, finalStepPage, documentsPage }) => {
+    test('TC_05_21_01 | Verify that button "Edit&Resend" is active', async ({ createBusinessUserAndLogin, signPage, prepareForSignatureModal, successModal, finalStepPage, documentsPage }) => {
 
-    test.setTimeout(250 * 1000);
-    await signPage.uploadFile.fileUploader.uploadFile('testDocuments/picture.jpg');
-    await signPage.uploadFile.clickPrepareDocumentBtn();
+        test.setTimeout(250 * 1000);
 
-    await prepareForSignatureModal.clickSendForSignatureRadioBtn();
-    await prepareForSignatureModal.clickAddSignerBtn();
-    await prepareForSignatureModal.fillSignerNameField(CHOOSE_SIGNERS_FIELDS.name1, 0)
-    await prepareForSignatureModal.fillSignerEmailField(process.env.PREFIX_EMAIL + '01' + process.env.EMAIL_DOMAIN, 0);
-    await prepareForSignatureModal.clickContinueBtn();
-    await prepareForSignatureModal.clickGotItBtn();
+        await prepareForSignature(signPage, prepareForSignatureModal);
+        await finalStepPage.waitAndClickSendForSignatureBtn();
+        await successModal.clickBackToDocumentsBtn();
+        await documentsPage.table.clickOptionsBtn();
+        await documentsPage.table.clickEditAndResendBtn();
 
-    await prepareForSignatureModal.clickSignFieldsItem();
-    await prepareForSignatureModal.doCanvasClicks();
-    await prepareForSignatureModal.clickSaveBtn();
+        await expect(documentsPage.table.titleEditAndResendDocument).toBeVisible();
+        expect(await documentsPage.table.getTitleText()).toBe("Edit & Resend document");
 
-    await finalStepPage.waitAndClickSendForSignatureBtn();
-    await successModal.clickBackToDocumentsBtn();
-    await documentsPage.table.clickOptionsBtn();
-    await documentsPage.table.clickEditAndResendBtn();
-
-    await expect(documentsPage.table.titleEditAndResendDocument).toBeVisible();
-    expect(await documentsPage.table.getTitleText()).toBe("Edit & Resend document");
-
-  })
+    })
 })
 
