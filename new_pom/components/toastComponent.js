36faf883a119b8294dd004d730a1f6efcf17b8e7
Eduard-QA <eduard.mickel@gmail.com>
2024-06-22 08:34:29 +0300

    new format (4 Spaces)

--- new_pom/components/toastComponent.js
+++ new_pom/components/toastComponent.js
@@ -2,8 +2,7 @@ export default class ToastComponent {
     constructor(page) {
         this.page = page;
 
-        this.toastFirstCloseBtn = this.page
-            .locator('.Toastify [data-src="/static/media/close-icon.9052da34.svg"]').first();
+        this.toastFirstCloseBtn = this.page.locator('.Toastify [data-src="/static/media/close-icon.9052da34.svg"]').first();
         this.toastBody = this.page.locator('.Toastify__toast-body');
 
     }
--- new_pom/modalWindows/successModal.js
+++ new_pom/modalWindows/successModal.js
@@ -1,15 +1,15 @@
 export default class SuccessModal {
-  constructor(page) {
-    this.page = page;
+    constructor(page) {
+        this.page = page;
 
-    this.statusDocument = this.page.getByText('Save a copy of your document');
-    this.backToDocumentsBtn = this.page.getByRole('button', { name: 'Back to Documents ' });
-  }
+        this.statusDocument = this.page.getByText('Save a copy of your document');
+        this.backToDocumentsBtn = this.page.getByRole('button', { name: 'Back to Documents ' });
+    }
 
-  async clickBackToDocumentsBtn() {
-    await this.statusDocument.waitFor({ state: 'visible' });
-    await new Promise(resolve => setTimeout(resolve, 1000));
-    await this.backToDocumentsBtn.click();
-  }
+    async clickBackToDocumentsBtn() {
+        await this.statusDocument.waitFor({ state: 'visible' });
+        await new Promise(resolve => setTimeout(resolve, 1000));
+        await this.backToDocumentsBtn.click();
+    }
 
 }
