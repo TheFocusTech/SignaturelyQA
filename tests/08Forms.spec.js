40ce27c60cfdab82191f6cc9022c5cef758dd9bc
Elena Nov <93733106+ElNov686@users.noreply.github.com>
2024-06-24 21:34:13 +0300

    allure tages fix

--- tests/08Forms.spec.js
+++ tests/08Forms.spec.js
@@ -2,24 +2,24 @@ import { expect } from "@playwright/test";
 import { test } from "../fixtures/base.js";
 import { SIGNERS_DATA, TOAST_MESSAGE, DOCUMENT_STATUS } from "../testData.js";
 import { createForm } from "../helpers/preconditions.js";
-import {allure} from "allure-playwright";
-import {Severity} from "allure-js-commons";
+import {description, tag, severity, Severity, link, epic, feature} from "allure-js-commons";
 
 test.describe('Sign Document', () => {
 
   test('TC_08_32_01 | Verify that user can create form', async ({ createBusinessUserAndLogin, signPage, 
     prepareForSignatureModal, createFormPage, formsPage, successModal }) => {
-    await allure.description('Verify that user can create form');
-    await allure.tags('Create_Form');
-    await allure.severity(Severity.CRITICAL);
-    await allure.link(
+    await description('Verify that user can create form');
+    await tag("Create Form");
+    await severity(Severity.CRITICAL);
+    await link(
               "Documentation",
               "https://docs.google.com/spreadsheets/d/1v5LuJ23jSg5qcWZPqiSlBuEJnvvdsJ2HrVxC6Ag2vpA/edit?gid=331265622#gid=331265622&range=A44",
               "TC_08_32_01"
           );
-    await allure.epic('Forms');
-    test.setTimeout(120 * 1000);
+    await epic("Forms");
+    await feature("Sign Document");
 
+    test.setTimeout(120 * 1000);
     await signPage.sideMenu.clickForms();
 
     await formsPage.clickCreateFormBtn();
@@ -43,19 +43,20 @@ test.describe('Sign Document', () => {
     await expect(await formsPage.table.documentStatus).toHaveText(DOCUMENT_STATUS.live);
   })
 
-  test.only('TC_08_35_01 | Verify that user can duplicate form', async ({ createBusinessUserAndLogin, signPage,  
+  test('TC_08_35_01 | Verify that user can duplicate form', async ({ createBusinessUserAndLogin, signPage,  
     prepareForSignatureModal, createFormPage, formsPage, successModal }) => {
-    await allure.description('Verify that user can duplicate form');
-    await allure.tags('Duplicate_Form');
-    await allure.severity(Severity.CRITICAL);
-    await allure.link(
+    await description('Verify that user can duplicate form');
+    await tags('Duplicate Form');
+    await severity(Severity.CRITICAL);
+    await link(
           "Documentation",
           "https://docs.google.com/spreadsheets/d/1v5LuJ23jSg5qcWZPqiSlBuEJnvvdsJ2HrVxC6Ag2vpA/edit?gid=331265622#gid=331265622&range=A47",
           "TC_08_35_01"
       );
-    await allure.epic('Forms');
-    test.setTimeout(120 * 1000);
+    await epic("Forms");
+    await feature("Sign Document");
 
+    test.setTimeout(120 * 1000);
     await createForm(signPage, prepareForSignatureModal, createFormPage, formsPage, successModal);
 
     await formsPage.table.clickOptionsBtn(1);
