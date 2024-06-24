6929374dc0cd59edc81d1214cf7f0b426be9bf26
Natalia Vasylieva <123753026+NatalyaVasylyeva@users.noreply.github.com>
2024-06-24 19:19:15 +0200

    RF TC_05_18_01 | Verify moving a document to a folder Add allure (#108)
    * TC_05_18_01 | Verify moving a document to a folder

--- new_pom/components/sideMenuComponent.js
+++ new_pom/components/sideMenuComponent.js
@@ -1,4 +1,5 @@
 import {allure} from "allure-playwright";
+import { test } from "../../fixtures/base";
 
 export default class SideMenuComponent {
     constructor(page) {
@@ -18,7 +19,9 @@ export default class SideMenuComponent {
     }
 
     async clickDocuments() {
-        await this.documents.click();
+        await test.step('Click the "Documents" item on the left SideMenu', async () => {
+            await this.documents.click();
+        });
     }
 
     async clickTemplates() {
