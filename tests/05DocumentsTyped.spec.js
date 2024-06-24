47d48a3f8c9dd657fcf50a2c4810441611be517f
Jul-Sol-RS <batareehkas@gmail.com>
2024-06-24 23:51:23 +0500

    Разрешение конфликта

--- tests/05DocumentsTyped.spec.js
+++ tests/05DocumentsTyped.spec.js
@@ -1,11 +1,7 @@
 import { expect } from "@playwright/test";
 import { test } from "../fixtures/base.js";
-import { CHOOSE_SIGNERS_FIELDS } from '../testData.js';
-import { createCompletedField } from '../helpers/preconditions.js';
 import { SIGNERS_DATA, UPLOAD_FILE_PATH, UPLOAD_FILE_NAME, FOLDER_NAME, TOAST_MESSAGE } from "../testData.js";
-import { createFolder } from "../helpers/preconditions.js";
-import { allure } from "allure-playwright";
-import { Severity } from "allure-js-commons";
+import { createFolder  } from "../helpers/preconditions.js";
 
 test.describe('DocumentsType', () => {
 
@@ -37,23 +33,13 @@ test.describe('DocumentsType', () => {
 
     });
 
-    test('TC_05_18_01 | Verify moving a document to a folder', async ({
+    test('TC_05_18_01 | Verify Move document to folder', async ({
         createBusinessUserAndLogin,
         signPage,
         documentsPage,
         moveToFolderModal,
         createFolderModal }) => {
         test.slow();
-
-        await allure.description('To verify the process of moving a document into a folder.');
-        await allure.tags('Move_to_folder');
-        await allure.severity(Severity.CRITICAL);
-        await allure.link(
-            "Documentation",
-            "https://docs.google.com/document/d/1Qce7tKWOwVYtPxgQv_8ae-HUkbAgeOFph0lB_eziY_k/edit#heading=h.ylpnl5bdm86k",
-            "TC_05_18_01"
-        );
-        await allure.epic('Documents (typed)');
 
         await createFolder(
             signPage,
@@ -66,29 +52,25 @@ test.describe('DocumentsType', () => {
         await documentsPage.table.clickMoveToBtn();
         await moveToFolderModal.selectFolder(FOLDER_NAME);
         await moveToFolderModal.clickMoveToFolderBtn();
-   
-        await test.step('Verify the toast message', async () => {
-            await expect(await documentsPage.toast.toastBody).toHaveText(TOAST_MESSAGE.fileMovedToFolder);
-        
+
+        await expect(await documentsPage.toast.toastBody).toHaveText(TOAST_MESSAGE.fileMovedToFolder);
+
         await documentsPage.table.openFolder(FOLDER_NAME);
-        await test.step('Verify the document is inside the folder', async () => {
-            await expect(await documentsPage.table.documentTitle).toHaveText(UPLOAD_FILE_NAME.jpgDocument);
-            })
-        })
-    });
+        await expect(await documentsPage.table.documentTitle).toHaveText(UPLOAD_FILE_NAME.jpgDocument);
+    })
+    test('TC_05_17_01 | Share document', async ({createBusinessUserAndLogin, signPage, prepareForSignatureModal, chooseSignatureOrInitialModal, finalStepPage, successModal, documentsPage, shareThisDocumentModal}) => {   
+        test.slow();   
+        await createCompletedField(signPage, prepareForSignatureModal, chooseSignatureOrInitialModal, finalStepPage, successModal);
+            
+        await documentsPage.sideMenuDocuments.clickCompletedLink();
+        await documentsPage.table.clickOptionsBtn();
+        await documentsPage.table.clickOptionsShareDropdown();
+    
+        await shareThisDocumentModal.clickTextField();
+        await shareThisDocumentModal.clickInputEmailField(SIGNERS_DATA.signerEmail1, 0);
+        await shareThisDocumentModal.clickShareDocumentBtn();
+    
+        await expect(documentsPage.toast.documentToast).toHaveClass('Toastify__toast-body');           
+      })
 
-   test('TC_05_17_01 | Share document', async ({createBusinessUserAndLogin, signPage, prepareForSignatureModal, chooseSignatureOrInitialModal, finalStepPage, successModal, documentsPage, shareThisDocumentModal}) => {   
-    test.slow();   
-    await createCompletedField(signPage, prepareForSignatureModal, chooseSignatureOrInitialModal, finalStepPage, successModal);
-        
-    await documentsPage.sideMenuDocuments.clickCompletedLink();
-    await documentsPage.table.clickOptionsBtn();
-    await documentsPage.table.clickOptionsShareDropdown();
-
-    await shareThisDocumentModal.clickTextField();
-    await shareThisDocumentModal.clickInputEmailField(CHOOSE_SIGNERS_FIELDS.email1);
-    await shareThisDocumentModal.clickShareDocumentBtn();
-
-    await expect(documentsPage.toast.documentToast).toHaveClass('Toastify__toast-body');           
-  })
 });
