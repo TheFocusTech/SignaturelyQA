c1f1286416ca9e9d86ed6d381e7ea41d74e383ae
NataliiaBor1 <borisenko.nataliia1@gmail.com>
2024-06-23 20:12:47 -0700

    TC_13_51_01 was refactored to new POM

--- new_pom/pages/settings/settingsEditSignaturePage.js
+++ new_pom/pages/settings/settingsEditSignaturePage.js
@@ -1,12 +1,15 @@
 import SideMenuSettingsComponent from "../../components/sideMenuSettingsComponent";
+import ToastComponent from "../../components/toastComponent";
 
 export default class NewSettingsEditSignaturePage {
     constructor(page) {
         this.page = page;
 
         this.sideMenuSettings = new SideMenuSettingsComponent(this.page);
+        this.toast = new ToastComponent(this.page);
         
         this.createSignatureBtn = this.page.getByRole('button', {name: 'Create Signature'});
+        this.settingsSignatureList = this.page.locator('.settingsSignature__list');
     }
 
     async clickCreateSignatureBtn() {
