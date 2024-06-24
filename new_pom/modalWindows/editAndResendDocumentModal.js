b5422af9b57792876733b82bc30abc9b5ea9a51e
Eduard-QA <eduard.mickel@gmail.com>
2024-06-24 06:11:02 +0300

    fixed spaces

--- new_pom/modalWindows/editAndResendDocumentModal.js
+++ new_pom/modalWindows/editAndResendDocumentModal.js
@@ -1,20 +1,20 @@
 export default class EditAndResendDocumentModal {
-  constructor(page) {
-      this.page = page;
+    constructor(page) {
+        this.page = page;
 
-      this.editAndResendTitle = this.page.getByText('Edit & Resend document');
-      this.revertToDraftBtn = this.page.getByRole('button', { name: 'Revert to Draft' });
-      
-  }
+        this.editAndResendTitle = this.page.getByText('Edit & Resend document');
+        this.revertToDraftBtn = this.page.getByRole('button', { name: 'Revert to Draft' });
 
-  async getTitleText() {
-    const actualText = await this.editAndResendTitle.textContent();
-    return actualText
-}
+    }
 
-async clickRevertToDraftBtn() {
-  await this.revertToDraftBtn.waitFor();
-  await this.revertToDraftBtn.click();
-}
+    async getTitleText() {
+        const actualText = await this.editAndResendTitle.textContent();
+        return actualText
+    }
+
+    async clickRevertToDraftBtn() {
+        await this.revertToDraftBtn.waitFor();
+        await this.revertToDraftBtn.click();
+    }
 
 }
