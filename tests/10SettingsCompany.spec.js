416fb9f8c99b46c95516dfa87fe224fff915f694
alfiyaGumar <alfiia.gumar@gmail.com>
2024-06-25 16:02:27 -0400

    fix

--- tests/10SettingsCompany.spec.js
+++ tests/10SettingsCompany.spec.js
@@ -6,10 +6,11 @@ import { allure } from "allure-playwright";
 import {Severity} from "allure-js-commons";
 
 test.describe('Company', () => {
-
+    
     test("TC_10_43_01 | Verify that the business user can fill company's form", async ({
         createBusinessUserAndLogin, signPage, settingsCompanyPage }) => {
-            await allure.description("To verify the process of filling company form.");
+       
+        await allure.description("To verify the process of filling company form.");
         await allure.tags('Fill company form');
         await allure.severity(Severity.CRITICAL);
         await allure.link(
@@ -31,6 +32,18 @@ test.describe('Company', () => {
         
         await test.step('Verify that toas "Company information successfully saved" appears', async () =>{
             await expect(settingsCompanyPage.toastPopup).toHaveText(TOAST_MESSAGE.companyInformationSave);
+
+        const actualCompanyName = await settingsCompanyPage.companyName.inputValue();
+        const actualEmailClosingSignature = await settingsCompanyPage.emailClosingSignature.inputValue();
+        const actualFromEmailName = await settingsCompanyPage.fromEmailName.inputValue();
+        const actualRedirectionPage = await settingsCompanyPage.redirectionPage.inputValue();
+        
+        await test.step('Verify that that the entered information is displayed correctly after saving', async () =>{
+            expect(actualCompanyName).toEqual(COMPANY_INFO.companyName);
+            expect(actualEmailClosingSignature).toEqual(COMPANY_INFO.emailTo);
+            expect(actualFromEmailName).toEqual(COMPANY_INFO.emailFrom);
+            expect(actualRedirectionPage).toEqual(COMPANY_INFO.redirectionPage);
         });
     });
+   });
 });
