63c03e3c1218a5fb6419ec20c0a4588db11c89a6
OlgaPerepelitzki <olga.perepelitzki2@gmail.com>
2024-06-26 20:41:56 +0300

    fix files problems

--- tests/14Billing.spec.js
+++ tests/14Billing.spec.js
@@ -1,11 +1,11 @@
 import { expect } from '@playwright/test';
 import { test } from "../fixtures/base.js";
-import {CARD_DETAILS, RANDOM_ANNUALLY_PLAN, PLANS, END_PLAN} from '../testData.js';
-import {description, tags, severity, Severity, link, epic, feature, step} from "allure-js-commons";
+import { CARD_DETAILS, RANDOM_ANNUALLY_PLAN, PLANS, END_PLAN } from '../testData.js';
+import { description, tags, severity, Severity, link, epic, feature, step } from "allure-js-commons";
 
 test.describe('Billing', () => {
 
-    test('TC_14_57_02 | Verify the ability to successfully downgrade subscription', async ({createBusinessUserAndLogin, signPage, settingsCompanyPage, settingsBillingPage, settingsBillingPlanPage, downgradeToPersonalPlanModal, specialOneTimeOfferModal}) => {
+    test('TC_14_57_02 | Verify the ability to successfully downgrade subscription', async ({ createBusinessUserAndLogin, signPage, settingsCompanyPage, settingsBillingPage, settingsBillingPlanPage, downgradeToPersonalPlanModal, specialOneTimeOfferModal }) => {
         await signPage.sideMenu.clickSettings();
         await settingsCompanyPage.sideMenuSettings.clickBilling();
         await settingsBillingPage.clickEditPlanButton();
@@ -19,7 +19,7 @@ test.describe('Billing', () => {
 
     test.describe('Upsell plan', () => {
         for (const plan of PLANS) {
-            test(`TC_14_56_01 | Verify successful upsell of users subscription ${plan} plan`, async ({createFreeUserAndLogin, signPage, settingsCompanyPage, settingsBillingPage, settingsBillingPlanPage, upgradeYourPlanModal, specialOneTimeOfferModal}) => {
+            test(`TC_14_56_01 | Verify successful upsell of users subscription ${plan} plan`, async ({ createFreeUserAndLogin, signPage, settingsCompanyPage, settingsBillingPage, settingsBillingPlanPage, upgradeYourPlanModal, specialOneTimeOfferModal }) => {
                 await signPage.sideMenu.clickSettings();
                 await settingsCompanyPage.horizontalMenu.clickBilling();
                 await settingsBillingPage.clickUpgradePlanButton();
@@ -28,22 +28,26 @@ test.describe('Billing', () => {
                 await upgradeYourPlanModal.clickSubscribeButton();
                 await specialOneTimeOfferModal.clickYesUpgradeMeBtn();
                 await expect(settingsBillingPlanPage.billingHeader).toContainText(RANDOM_ANNUALLY_PLAN(plan));
-        })
-       }
+            })
+        }
     })
 
     test('TC_14_54_01 | Attach/delete payment card', async ({
-                                                                createFreeUserAndLogin,
-                                                                signPage,
-                                                                settingsCompanyPage,
-                                                                settingsBillingPage,
-                                                            }) => {
+        createFreeUserAndLogin,
+        signPage,
+        settingsCompanyPage,
+        settingsBillingPage,
+    }) => {
         await description('Objective: To verify the functionality of attaching a payment card ' +
             'through the settings-billing section and deleting a payment card through the Billing Portal.')
         await severity(Severity.CRITICAL);
         await link(
+            'https://app.qase.io/case/SIGN-54',
+            'Qase: SIGN-54'
+        );
+        await link(
             'https://docs.google.com/document/d/1Qce7tKWOwVYtPxgQv_8ae-HUkbAgeOFph0lB_eziY_k/edit#heading=h.khucr6xuqdib',
-            'TC_14_54_01'
+            'ATC_14_54_01'
         );
         await epic('Setting');
         await feature('Billing');
