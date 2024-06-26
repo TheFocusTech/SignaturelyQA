36f5d76b4690d7af7a01753ae512e97bbd6f1ccf
Elena Nov <93733106+ElNov686@users.noreply.github.com>
2024-06-26 18:19:44 +0300

    qase link added

--- tests/08Forms.spec.js
+++ tests/08Forms.spec.js
@@ -4,10 +4,29 @@ import { SIGNERS_DATA, TOAST_MESSAGE, DOCUMENT_STATUS } from "../testData.js";
 import { createForm } from "../helpers/preconditions.js";
 import {description, tag, severity, Severity, link, epic, feature} from "allure-js-commons";
 
-test.describe('Sign Document', () => {
+test.describe('Forms', () => {
 
-  test('TC_08_32_01 | Verify that user can create form', async ({ createBusinessUserAndLogin, signPage,
-    prepareForSignatureModal, createFormPage, formsPage, successModal }) => {
+  test('TC_08_32_01 | Verify that user can create form', async ({ 
+        createBusinessUserAndLogin, 
+        signPage,
+        prepareForSignatureModal, 
+        createFormPage, 
+        formsPage, 
+        successModal }) => {
+        await description('Verify that user can create form');
+        await tag('Create Form');
+        await severity(Severity.CRITICAL);
+        await link(
+                'https://app.qase.io/case/SIGN-35',
+                'Qase: SIGN-5'
+            );
+        await link(
+          "https://docs.google.com/document/d/1Qce7tKWOwVYtPxgQv_8ae-HUkbAgeOFph0lB_eziY_k/edit#heading=h.c5mhxwvn5pxt",
+          "TC_08_35_01"
+      );
+        await epic("Forms");
+        await feature("Sign Document");
+        
 
         test.setTimeout(120 * 1000);
         await signPage.sideMenu.clickForms();
@@ -48,6 +67,10 @@ test.describe('Sign Document', () => {
         await description('Verify that user can duplicate form');
         await tag('Duplicate Form');
         await severity(Severity.CRITICAL);
+        await link(
+            'https://app.qase.io/case/SIGN-32',
+            'Qase: SIGN-5'
+        );
         await link(
           "https://docs.google.com/document/d/1Qce7tKWOwVYtPxgQv_8ae-HUkbAgeOFph0lB_eziY_k/edit#heading=h.c5mhxwvn5pxt",
           "TC_08_35_01"
