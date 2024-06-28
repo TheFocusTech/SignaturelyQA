383ebf502f4c0c7a466d03ebe4b3b2f5dd7a5b60
OlgaPerepelitzki <olga.perepelitzki2@gmail.com>
2024-06-28 13:54:41 +0300

    Fix problem in file CreateNewTemplatePage

--- new_pom/pages/templates/createNewTemplatePage.js
+++ new_pom/pages/templates/createNewTemplatePage.js
@@ -2,6 +2,7 @@ import SideMenuComponent from "../../components/sideMenuComponent";
 import SideMenuTemplatesComponent from "../../components/sideMenuTemplatesComponent";
 import FileUploaderComponent from "../../components/fileUploaderComponent";
 import CreateTemplateComponent from "../../components/createTemplateComponent";
+import { step } from "allure-js-commons";
 
 export default class CreateNewTemplatePage {
 
@@ -11,6 +12,11 @@ export default class CreateNewTemplatePage {
         this.sideMenuTemplates = new SideMenuTemplatesComponent(this.page);
         this.fileUploader = new FileUploaderComponent(this.page);
         this.createTemplate = new CreateTemplateComponent(this.page);
+
+        this.templateNameField = this.page.getByPlaceholder('A template name to identify');
+        this.optionalMessageField = this.page.getByPlaceholder('Add an optional message for');
+        this.createTemplateRolesField = this.page.getByPlaceholder('Role');
+        this.fillTemplateBtn = this.page.getByRole('button', { name: 'Fill Template' });
     }
     async fillTemplateNameField(name) {
         await step('Fill in the "Template Name" field', async () => {
