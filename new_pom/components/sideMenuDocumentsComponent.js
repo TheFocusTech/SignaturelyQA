6641586b771ff8ea344e59e03e1679ba054410d5
Anastasiia Go <132062068+Anastassyia@users.noreply.github.com>
2024-06-28 14:01:13 +0200

    TC_04_10_02 | Verify that the user who uploaded the document and other signer can sign it (#149)
    * TC_04_10_02 | Verify that the user who uploaded the document and other signer can sign it
    
    * Add allure
    
    * Add step impots
    
    * Add step imports v2
    
    * add step import
    
    * add link

--- new_pom/components/sideMenuDocumentsComponent.js
+++ new_pom/components/sideMenuDocumentsComponent.js
@@ -1,9 +1,11 @@
+import { step } from "allure-js-commons";
 export default class SideMenuDocumentsComponent {
     constructor(page) {
         this.page = page;
 
         this.trash = this.page.getByRole('link', { name: 'Trash' });
         this.awaitingSignature = this.page.getByRole('link', {name: 'Awaiting Signature'});
+        this.draft = this.page.getByRole('link', { name: 'Draft' });
 
     }
 
@@ -14,5 +16,10 @@ export default class SideMenuDocumentsComponent {
     async clickAwaitingSignature() {
         await this.awaitingSignature.click();
     }
-    
+
+    async clickDraft() {
+        await step('Click "Draft" submenu', async () => {
+            await this.draft.click();
+        })
+    }
 }
