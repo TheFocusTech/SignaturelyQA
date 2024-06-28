bc0143bead6c5c17abcaf5a4f2b920d9eccbe3fd
OlgaPerepelitzki <olga.perepelitzki2@gmail.com>
2024-06-28 13:00:52 +0300

    revers methods to the createNewTemplatePage

--- new_pom/pages/templates/createNewTemplatePage.js
+++ new_pom/pages/templates/createNewTemplatePage.js
@@ -12,5 +12,29 @@ export default class CreateNewTemplatePage {
         this.fileUploader = new FileUploaderComponent(this.page);
         this.createTemplate = new CreateTemplateComponent(this.page);
     }
+    async fillTemplateNameField(name) {
+        await step('Fill in the "Template Name" field', async () => {
+            await this.templateNameField.fill(name);
+        });
+    }
+
+    async fillOptionalMessageField(message) {
+        await step('Fill in the "Optional Message" field', async () => {
+            await this.optionalMessageField.fill(message);
+        });
+    }
+
+    async fillCreateTemplateRolesField(role) {
+        await step('Fill in the "Role" field', async () => {
+            await this.createTemplateRolesField.fill(role);
+        });
+    }
+
+    async clickFillTemplateBtn() {
+        await step('Click on the "Fill template" button', async () => {
+            await this.fillTemplateBtn.waitFor();
+            await this.fillTemplateBtn.click();
+        });
+    }
 
 }
