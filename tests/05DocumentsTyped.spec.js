be9ed9b820fb4ab374fc2b8e1077169086dde7d2
Iryna Kolhanova <iryna.kolhanova@gmail.com>
2024-06-28 23:15:31 -0400

    skip test

--- tests/05DocumentsTyped.spec.js
+++ tests/05DocumentsTyped.spec.js
@@ -80,7 +80,7 @@ test.describe('DocumentsType', () => {
         });
     });
 
-    test('TC_05_18_01 | Verify moving a document to a folder', async ({
+    test.skip('TC_05_18_01 | Verify moving a document to a folder', async ({
         createBusinessUserAndLogin,
         signPage,
         documentsPage,
