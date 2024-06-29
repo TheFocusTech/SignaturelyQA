86faead4dd1e8bcfbd9493cd470f9aeea99e459f
Elena D <lenadikov@gmail.com>
2024-06-29 19:45:10 -0400

    refactoring test

--- tests/06Folders.spec.js
+++ tests/06Folders.spec.js
@@ -1,7 +1,7 @@
 import { expect } from '@playwright/test';
 import { test } from '../fixtures/base.js';
-import { createFolder } from '../helpers/preconditions.js';
-import { TOAST_MESSAGE, FILL_RENAME_FOLDER_NAME, QASE_LINK, GOOGLE_DOC_LINK, } from '../testData.js';
+import { createFolder, createSecondFolder } from '../helpers/preconditions.js';
+import { TOAST_MESSAGE, FILL_RENAME_FOLDER_NAME, QASE_LINK, GOOGLE_DOC_LINK, FOLDER_NAME} from '../testData.js';
 import { description, tag, severity, Severity, link, epic, step } from "allure-js-commons";
 
 test.describe('Folders', () => {
@@ -64,50 +64,35 @@ test.describe('Folders', () => {
 
     });
 
-});
-    test.skip('TC_06_23_01 | Rename folder', async ({ page, createBusinessUserAndLogin, createNewFolder }) => {
-        const signPage = new SignPage(page); 
-
-        const documentsPage = await signPage.clickDocumentsSidebarLinkAndGoDocumentsPage();
-
-        await documentsPage.clickOptionsBtn();
-        await documentsPage.clickRenameBtn();
-        await documentsPage.fillRenameInputField(FILL_RENAME_FOLDER_NAME)
-        await documentsPage.pressEnterRenameInputFielder();
-
-        await expect(documentsPage.locators.getToast()).toHaveText(TOAST_MESSAGE.folderRename);
-    });
-
-    test('TC_06_25_01 | Move folder to folder', async ({ createBusinessUserAndLogin,
+    test.only('TC_06_25_01 | Move folder to folder', async ({ createBusinessUserAndLogin,
         signPage,
         documentsPage,
         createFolderModal,
         moveToFolderModal  }) => {
 
-            await description('To verify the user can move folder to folder');
-            await severity(Severity.CRITICAL);
-            await link('https://app.qase.io/case/SIGN-25', 'Qase: SIGN-25');
-            await link('https://docs.google.com/document/d/1Qce7tKWOwVYtPxgQv_8ae-HUkbAgeOFph0lB_eziY_k/edit#heading=h.2d0wcpkoa0jo', 'ATC_06_25_01');
-            await epic('Folder');
-            await tag('Move folder to folder');
+        await description('To verify the user can move folder to folder');
+        await tag('Move folder to folder');
+        await severity(Severity.CRITICAL);
+        await link(`${QASE_LINK}/SIGN-25`, 'Qase: SIGN-25');
+        await link(`${GOOGLE_DOC_LINK}2d0wcpkoa0jo`, 'ATC_06_25_01');
+        await epic('Folder');   
 
-            await createFolder(
-                signPage,
-                documentsPage,
-                createFolderModal);
-                
-            await createSecondFolder(
-                signPage,
-                documentsPage,
-                createFolderModal);
-    
-            await signPage.sideMenu.clickDocuments();
-            await documentsPage.table.clickOptionsBtn(0);
-            await documentsPage.table.clickMoveToBtn();
-            await moveToFolderModal.selectFolder(FOLDER_NAME);
-            await moveToFolderModal.clickMoveToFolderBtn();
+        await createFolder(
+            signPage,
+            documentsPage,
+            createFolderModal);            
+        await createSecondFolder(
+            signPage,
+            documentsPage,
+            createFolderModal);
+        await signPage.sideMenu.clickDocuments();
+        await documentsPage.table.clickFirstOptionsBtn();
+        await documentsPage.table.clickMoveToBtn();
+        await moveToFolderModal.selectFolder(FOLDER_NAME);
+        await moveToFolderModal.clickMoveToFolderBtn();
 
-            await step('Verify the toast message', async () => {
-                await expect(await documentsPage.toast.toastBody).toHaveText(TOAST_MESSAGE.folderMovedToFolder);
-            });
+        await step('Verify the toast message', async () => {
+            await expect(documentsPage.toast.toastBody).toHaveText(TOAST_MESSAGE.folderMovedToFolder);
+        });
     });
+});
