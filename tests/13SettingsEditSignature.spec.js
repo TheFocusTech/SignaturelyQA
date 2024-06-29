536bf2ec14b9b4941da2c64d99d741b0f96491a6
NataliiaBor1 <borisenko.nataliia1@gmail.com>
2024-06-29 16:53:52 -0700

    Allure annotations were added

--- tests/13SettingsEditSignature.spec.js
+++ tests/13SettingsEditSignature.spec.js
@@ -1,10 +1,23 @@
 import { expect } from "@playwright/test";
 import { test, createBusinessUserAndLogin } from "../fixtures/base.js";
-import { DATA_SIGNER, TOAST_MESSAGE } from "../testData.js";
+import { DATA_SIGNER, TOAST_MESSAGE, QASE_LINK, GOOGLE_DOC_LINK } from "../testData.js";
+import { description, tag, severity, Severity, link, epic, step } from 'allure-js-commons';
 
 test.describe('Settings: Edit signature', () => {
-
-    test('TC_13_51_01 | Verify that user can create Signature.', async ({ createBusinessUserAndLogin, signPage, settingsCompanyPage, settingsEditSignaturePage, createOrEditSignatureOnSettingModal }) => {
+    test('TC_13_51_01 | Verify that user can create Signature', async ({
+        createBusinessUserAndLogin,
+        signPage,
+        settingsCompanyPage,
+        settingsEditSignaturePage,
+        createOrEditSignatureOnSettingModal
+    }) => {
+        await description('Verify that the user can create a Signature.');
+        await severity(Severity.CRITICAL);
+        await link(`${QASE_LINK}/SIGN-51`, 'Qase: SIGN-51');
+        await link(`${GOOGLE_DOC_LINK}h.5bzz9ln1m6ek`, 'ATC_13_51_01');
+        await epic('Settings: Edit signature');
+        await tag('Create a signature');
+        
         await signPage.sideMenu.clickSettings();
         await settingsCompanyPage.sideMenuSettings.clickEditSignature();
         await settingsEditSignaturePage.clickCreateSignatureBtn();
