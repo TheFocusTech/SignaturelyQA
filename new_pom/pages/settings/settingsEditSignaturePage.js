5780face89b8cd3e6c60314f683090c3e2dcb378
Elena Nov <93733106+ElNov686@users.noreply.github.com>
2024-07-02 23:24:55 +0300

    third commit

--- new_pom/pages/settings/settingsEditSignaturePage.js
+++ new_pom/pages/settings/settingsEditSignaturePage.js
@@ -14,6 +14,7 @@ export default class SettingsEditSignaturePage {
         this.dropDownMenu = this.page.getByRole('listitem').locator('div').nth(2);;
         this.deleteSignatureDropDownItem = this.page.getByRole('listitem').locator('div').filter({hasText: 'Delete'}).nth(2);
         this.editSignatureDropDownItem = this.page.getByRole('listitem').locator('div').filter({hasText: 'Edit'}).nth(3);
+        this.signaturelyInner = this.page.locator('settingsSignature__item-inner');
     }
 
     async clickCreateSignatureBtn() {
