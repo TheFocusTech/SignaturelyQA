84258d973f27fb7315d9c44d39ba5b8364bda181
InaChemerko <34387983+InaChemerko@users.noreply.github.com>
2024-07-01 22:00:28 -0700

    TC_09_38_01_POM_Allure (#188)
    * Added test

--- tests/09Team.spec.js
+++ tests/09Team.spec.js
@@ -11,6 +11,63 @@ import { retrieveUserEmailConfirmationLink } from '../helpers/utils.js';
 import { description, tag, severity, Severity, link, epic, step } from 'allure-js-commons';
 
 test.describe('Team', () => {
+    test('TC_09_38_01 | Verify that Business User can add team member', async ({
+        page,
+        request,
+        createBusinessUserAndLogin,
+        signPage,
+        teamPage,
+        addTeamMemberModal,
+        teamsAcceptInvitePage,
+    }) => {
+        await description('Objective: To verify that Business User can add the teammate');
+        await severity(Severity.CRITICAL);
+        await link(`${QASE_LINK}/SIGN-38`, 'Qase: SIGN-38');
+        await link(`${GOOGLE_DOC_LINK}5trza9yu39er`, 'ATC_09_38_01');
+
+        await epic('Team');
+        await tag('Team Member');
+
+        test.setTimeout(90000);
+
+        const teamMemberEmail = `${process.env.EMAIL_PREFIX}${process.env.NEW_USER_NUMBER}${'_teammember'}${
+            process.env.EMAIL_DOMAIN
+        }`;
+        const teamMemberName = `${process.env.NEW_USER_NAME}${'_teammember'}`;
+
+        await signPage.sideMenu.clickTeam();
+        await teamPage.clickAddTeamMemberButton();
+        await addTeamMemberModal.fillTeamMemberEmailInputField(teamMemberEmail);
+        await addTeamMemberModal.fillTeamMemberNameInputField(teamMemberName); 
+        (await addTeamMemberModal.isTeamMemberRoleSet(TEAM_MEMBER_ROLES.user))
+            ? null
+            : await addTeamMemberModal.changeTeamMemberRole(TEAM_MEMBER_ROLES.user);       
+        await addTeamMemberModal.clickSendInvitesButton();
+
+        await step('Verify that a toast message ‘Invites sent successfully’ popped up', async () => {
+            await expect(teamPage.toast.toastBody).toHaveText(TOAST_MESSAGE.invitesSent);
+        });
+
+        const emailSubject = `${process.env.NEW_USER_NAME}${EMAIL_SUBJECTS.inviteToJoin}`;
+        const inviteLink = await retrieveUserEmailConfirmationLink(request, teamMemberEmail, emailSubject);
+        await step('Navigate to the invite link', async () => {
+            await page.goto(inviteLink);
+        });
+        await teamsAcceptInvitePage.clickBackToMainPageButton();
+        await teamsAcceptInvitePage.toast.waitForToastIsHiddenByText(TOAST_MESSAGE.inviteAccepted);
+        await signPage.sideMenu.clickTeam();
+
+        await step("Verify that a team member is displayed in the Team table", async () => {
+            await expect(await teamPage.teamMemberRoleForExactTeamMemeber(teamMemberEmail)).toBeVisible();              
+        });
+
+        await step("Verify that a team member has role 'User' set in the Team table", async () => {
+            await expect(await teamPage.teamMemberRoleForExactTeamMemeber(teamMemberEmail)).toHaveText(
+                TEAM_MEMBER_ROLES.user
+            );
+        });        
+    });
+
     test('TC_09_38_02 | Verify that Business User can add "Admin" team member', async ({
         page,
         request,
