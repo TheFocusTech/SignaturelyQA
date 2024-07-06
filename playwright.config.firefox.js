3bc6b3eca777af26de99a6b17527b636ccf77ab3
Svetlana Kanel <svetlana.kanel@gmail.com>
2024-07-06 14:21:56 -0400

    Add config.firefox

--- playwright.config.firefox.js
+++ playwright.config.firefox.js
@@ -26,8 +26,8 @@ module.exports = defineConfig({
     // reporter: 'html',
     grep: testPlanFilter(),
     reporter: [
-        ["list"],        
-        ['html', { outputFolder: 'my-reportTest-Chrome' }],
+        ["list"],
+        ['html', { outputFolder: 'my-reportTest-Firefox' }],
         [
             "allure-playwright",
             {
@@ -51,7 +51,7 @@ module.exports = defineConfig({
     /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
     timeout: 60000,
     use: {
-        permissions: ['clipboard-write', 'clipboard-read'],
+       // permissions: ['clipboard-write', 'clipboard-read'],
 
         headless: process.env.CI ? true : false,
 
@@ -67,40 +67,12 @@ module.exports = defineConfig({
 
     /* Configure projects for major browsers */
     projects: [
+     
         {
-            name: 'chromium',
-            use: {...devices['Desktop Chrome']},
-        },
-
-        // {
-        //   name: 'firefox',
-        //   use: { ...devices['Desktop Firefox'] },
-        // },
-
-        // {
-        //   name: 'webkit',
-        //   use: { ...devices['Desktop Safari'] },
-        // },
-
-        /* Test against mobile viewports. */
-        // {
-        //   name: 'Mobile Chrome',
-        //   use: { ...devices['Pixel 5'] },
-        // },
-        // {
-        //   name: 'Mobile Safari',
-        //   use: { ...devices['iPhone 12'] },
-        // },
-
-        /* Test against branded browsers. */
-        // {
-        //   name: 'Microsoft Edge',
-        //   use: { ...devices['Desktop Edge'], channel: 'msedge' },
-        // },
-        // {
-        //   name: 'Google Chrome',
-        //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
-        // },
+          name: 'firefox',
+          use: { ...devices['Desktop Firefox'] },
+        }
+  
     ],
 
     /* Run your local dev server before starting the tests */
