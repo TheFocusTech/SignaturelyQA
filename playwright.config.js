2891e91921a7e409174fb108376aa752a831ca3f
Svetlana Kanel <svetlana.kanel@gmail.com>
2024-07-05 23:46:27 -0400

    Add browsers

--- playwright.config.js
+++ playwright.config.js
@@ -21,7 +21,7 @@ module.exports = defineConfig({
     /* Retry on CI only */
     retries: process.env.CI ? 2 : 0,
     /* Opt out of parallel tests on CI. */
-    workers: process.env.CI ? 1 : 1,
+    workers: process.env.CI ? 4 : undefined,
     /* Reporter to use. See https://playwright.dev/docs/test-reporters */
     // reporter: 'html',
     grep: testPlanFilter(),
@@ -72,15 +72,15 @@ module.exports = defineConfig({
             use: {...devices['Desktop Chrome']},
         },
 
-        // {
-        //   name: 'firefox',
-        //   use: { ...devices['Desktop Firefox'] },
-        // },
+        {
+          name: 'firefox',
+          use: { ...devices['Desktop Firefox'] },
+        },
 
-        // {
-        //   name: 'webkit',
-        //   use: { ...devices['Desktop Safari'] },
-        // },
+        {
+          name: 'webkit',
+          use: { ...devices['Desktop Safari'] },
+        },
 
         /* Test against mobile viewports. */
         // {
